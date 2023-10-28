ecb9cc5d35757450112d351af53b430a712a7164
Mariog3497 <mariog3497@gmail.com>
2023-10-28 16:35:14 +0200

    Gitignore

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+https://www.medium.commlearning-aiapply-machine-learning-algorithms-for-genomics-data-classification-132972933723
+https://www.nature.com/articles/s41588-018-0295-5
+https://github.com/abidlabs/deep-learning-genomics-primer/blob/master/A_Primer_on_Deep_Learning_in_Genomics_Public.ipynb
+"""
+
+print(123)

26d13b67d3142ff24bd3dff7645eacd87c95c91c
Mariog3497 <mariog3497@gmail.com>
2023-11-09 18:10:53 +0100

    EDA CONTINUES

--- eda_functions.py
+++ eda_functions.py
@@ -1,6 +1,23 @@
 import pandas as pd
 
 def load_data(seq_path='data/sequences.txt', labels_path='data/labels.txt'):
+    # Read txt files from data directory, default path is given
     df_seqs = pd.read_csv(seq_path, header=None)
     df_labels = pd.read_csv(labels_path, header=None)
     return df_seqs, df_labels
+
+def check_is_dna(dna_string):
+    # Check if all nucleotides in the input dna_string are present in the DNA alphabet
+    DNA_nucleotides = set('ACTG')
+    return all(base in DNA_nucleotides for base in set(dna_string))
+
+def drop_non_dna(df_seqs):
+    # Check which strings are DNA
+    dna_strings = df_seqs[0].apply(check_is_dna)
+    # If all strings are DNA then we return the same df
+    if sum(dna_strings)==len(df_seqs):
+        return df_seqs
+    # If not we drop non DNA strings and return the filtered df
+    else:
+        return df_seqs.drop(dna_strings[dna_strings==False].index,axis=0)
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 https://www.medium.commlearning-aiapply-machine-learning-algorithms-for-genomics-data-classification-132972933723
 https://www.nature.com/articles/s41588-018-0295-5
 https://github.com/abidlabs/deep-learning-genomics-primer/blob/master/A_Primer_on_Deep_Learning_in_Genomics_Public.ipynb
+http://blog.hadsonpar.com/2022/11/iniciar-entorno-de-trabajo-desde.html
 """
 
 print(123)

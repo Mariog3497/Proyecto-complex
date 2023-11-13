d32031065ee74fbc1216032db6203c6e620feb49
Mariog3497 <mariog3497@gmail.com>
2023-11-13 18:09:05 +0100

    Histogram and encoding completed

--- eda_functions.py
+++ eda_functions.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
 
 def load_data(seq_path='data/sequences.txt', labels_path='data/labels.txt'):
     # Read txt files from data directory, default path is given
@@ -20,4 +22,24 @@ def drop_non_dna(df_seqs):
     # If not we drop non DNA strings and return the filtered df
     else:
         return df_seqs.drop(dna_strings[dna_strings==False].index,axis=0)
+    
+def plot_nucleotides(df):
+    # Join all sequences into a single string
+    all_seqs = ''.join(df[0].values)
+
+    # Count how many nucleotides of each type we have
+    conts = Counter(all_seqs)
+
+    # Get percentages
+    nucleotides_pct = dict((k, round(v/len(all_seqs)*100,2)) for k, v in conts.items())
+
+    # Order it alphabetically
+    nucleotides_pct = dict(sorted(nucleotides_pct.items()))
+
+    # Plot a histogram
+    plt.bar(nucleotides_pct.keys(), nucleotides_pct.values())
+    plt.title('Percentage of each nueclotide occurence in our dataset')
+    plt.ylabel('Percentage (%)')
+    plt.xlabel('Nucleotide')
+    plt.plot()
 
